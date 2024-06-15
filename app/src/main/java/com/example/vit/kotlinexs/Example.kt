6ff7933d6cdd9b54f246d20052a52906df4c3c12
Asus <khannaujjwal2003@gmail.com>
2024-06-16 00:05:11 +0530

    how to add colours

--- app/src/main/java/com/example/vit/kotlinexs/Example.kt
+++ app/src/main/java/com/example/vit/kotlinexs/Example.kt
@@ -1,8 +1,14 @@
 package com.example.vit.kotlinexs
 
 fun main(){
+    var myTeacher: Teacher= Teacher(name = "Abdul", age = 121, postalAddress = "abc")
     var myStudent: Student=Student(name = "Ujjwal", age = 21, postalAddress = "kolkata")
     println(myStudent.name)
     myStudent.name="Khanna"
     println(myStudent.name)
+    println(myTeacher.age)
+    myStudent.name="Ansari"
+    println(myStudent.name)
+
+
 }
--- app/src/main/java/com/example/vit/kotlinexs/Student.kt
+++ app/src/main/java/com/example/vit/kotlinexs/Student.kt
@@ -3,6 +3,6 @@ package com.example.vit.kotlinexs
 class Student(var name: String, var age: Int, var postalAddress: String)
 
 
-
+//here java code has been converted to kotlin code
 
 
