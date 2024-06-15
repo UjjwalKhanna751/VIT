4883e5292696b99e26cea93321ebf73d000760c2
Asus <khannaujjwal2003@gmail.com>
2024-06-16 00:06:21 +0530

    how to add colours

---
+++ app/src/main/java/com/example/vit/kotlinexs/model.kt
@@ -0,0 +1,9 @@
+package com.example.vit.kotlinexs
+
+class Department(var name: String, var age: Int, var postalAddress: String)
+
+class College(var name: String, var age: Int, var postalAddress: String)
+
+class Teacher(var name: String, var age: Int, var postalAddress: String)
+
+//no need to create more than one files, only one demo file in same package can be used
