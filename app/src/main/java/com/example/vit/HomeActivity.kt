4d35167d9c45aaa9531dbb573a7203c60df17d32
Asus <khannaujjwal2003@gmail.com>
2024-06-23 23:10:43 +0530

    passing data to another activity

---
+++ app/src/main/java/com/example/vit/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.example.vit
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var data = intent.extras?.getString("nkey")
+        Log.i("Home Acttivity","Button was clicked"+ data)
+        var homeTextView: TextView = findViewById(R.id.tvhome)
+        homeTextView.setText(data)
+    }
+
+}
